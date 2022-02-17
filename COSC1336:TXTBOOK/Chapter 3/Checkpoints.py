a81fce39f640cc068e6e9ab5e801a4aca48ae2ae
rObErT-a93 <robert.anderson11@g.austincc.edu>
2022-02-17 13:03:14 -0600

    Checkpoints Ch. 3

---
+++ COSC1336:TXTBOOK/Chapter 3/Checkpoints.py
@@ -0,0 +1,32 @@
+#3.11 What would the following code display?
+if 'z' < 'a':
+    print('z is less than a')
+else:
+    print('z is not less than a')
+print()
+#3.12 What would the following code display?
+s1 = 'New York'
+s2 = 'Boston'
+if s1 > s2:
+    print(s2)
+    print(s1)
+else:
+    print(s1)
+    print(s2)
+print()
+
+#3.18 Write an 'if' statement that displays the message "the number is valid"
+# if the the value referenced by "speed" is w/n the range 0 thru 200
+speed = 175
+if speed >= 0 and speed <= 200:
+    print('The number is valid')
+else:
+    print('The number is not valid')
+print()
+#3.19 Write an 'if' statement that displays the message "The number is not valid"
+# if the value referenced by "speed" is outside the range 0 thru 200
+speed2 = 201
+if speed2 >= 0 and speed2 <= 200:
+    print('The number is valid')
+else:
+    print('The number is not valid')
