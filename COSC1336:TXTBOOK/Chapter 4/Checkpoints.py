e8244d52a01804c29f7207be29109fe8040a69b5
rObErT-a93 <robert.anderson11@g.austincc.edu>
2022-02-24 15:19:24 -0600

    Chapter 4 2/24

--- COSC1336:TXTBOOK/Chapter 4/Checkpoints.py
+++ COSC1336:TXTBOOK/Chapter 4/Checkpoints.py
@@ -5,5 +5,22 @@
 count = 10
 while count < 1:
     print('Hello World')
-
+print()
 # The output does not return a value due to the '<' in while clause.
+
+# Checkpoint 4.15
+# What will the following code display?
+
+total = 0
+for count in range(1,6):
+    total = total + count
+print(total)
+print()
+# Checkpoint 4.16
+# What will the following code display?
+
+number1 = 10
+number2 = 5
+number1 += number2  # same expression as = number1 + number2
+print(number1)
+print(number2)
