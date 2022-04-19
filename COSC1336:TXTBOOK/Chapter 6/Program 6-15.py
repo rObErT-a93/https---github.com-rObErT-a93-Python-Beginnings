fe9ed3d27a489b416d23f1298fa78134c108ecc2
rObErT-a93 <robert.anderson11@g.austincc.edu>
2022-04-19 11:53:03 -0500

    Chapter 6 4/19

---
+++ COSC1336:TXTBOOK/Chapter 6/Program 6-15.py
@@ -0,0 +1,32 @@
+# This program adds coffee inventory records to
+# the coffee.txt file.
+
+def main():
+    # Create a variable to control the loop.
+    another = 'y'
+
+    # Open the coffee.txt file in append mode.
+    coffee_file = open('Coffee.txt', 'a')
+
+    # Add records to the file.
+    while another == 'y' or another == 'Y':
+        # Get the coffee record data.
+        print('Enter the following coffee data:')
+        descr = input('Description: ')
+        qty = float(input('Quanntity (in pounds): '))
+
+        # Append the data to the file.
+        coffee_file.write(descr + '\n')
+        coffee_file.write(str(qty) + '\n')
+
+        # Determine whether the user wants to add
+        # another record to the file.
+        print('Do you want to add another recortd?')
+        another = input('Y = yes, anything else = no: ')
+
+    # Close the file.
+    coffee_file.close()
+    print('Data appended to coffee.txt.')
+
+# Call the main function.
+main()
